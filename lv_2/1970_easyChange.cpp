c94ba289307c17a34168281e2da3df1f81576835
MuRing <11esh@naver.com>
2022-11-15 21:21:51 +0900

    easy

--- lv_2/1970_easyChange.cpp
+++ lv_2/1970_easyChange.cpp
@@ -4,13 +4,32 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    int N;
-    cin >> N;
+    int test_case;
+	int T;
+	cin>>T;
+	
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        int N;
+        cin >> N;
 
-    // 50000 10000 5000 1000 500 100 50 10
-    int ans[8] ={0, };
+        // 50000 10000 5000 1000 500 100 50 10
+        int arr[8] = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
+        int ans[8] = {0, };
 
-    
-
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+        for(int i = 0; i < 8; i++){
+            int temp;
+            if(N / arr[i] != 0){
+                temp = N / arr[i];
+                ans[i] = temp;
+                N %= arr[i];
+            }
+        }
+        
+        cout << "#" << test_case << endl;
+        for(int i = 0; i < 8; i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+    return 0;
 }
