4f77d360cb3dafdd51a66aadbf901d23b53aee33
MuRing <11esh@naver.com>
2022-11-16 23:55:10 +0900

    array rotation

---
+++ lv_2/1961_NumberArrayRotation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+	int T;
+	cin>>T;
+	
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        int N;
+        cin >> N;
+
+        
+    }
+    return 0;
+}
