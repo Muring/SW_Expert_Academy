0996a4d787bf62c142673267d72e6ceca4915650
MuRing <11esh@naver.com>
2022-11-18 23:43:14 +0900

    BFS

---
+++ lv_3/14413_ColoringBoard.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+	int T;
+	cin>>T;
+	
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+        
+
+        cout << "#" << test_case << " " << count << endl;
+    }
+    return 0;
+}
