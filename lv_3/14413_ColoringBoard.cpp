54d60d40c73a689ab068b3e8f610bf1172e99c07
MuRing <11esh@naver.com>
2022-11-20 04:23:20 +0900

    Map and board concept

--- lv_3/14413_ColoringBoard.cpp
+++ lv_3/14413_ColoringBoard.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <vector>
-
+ 
 using namespace std;
-
+ 
 int main(int argc, char** argv)
 {
     int test_case;
-	int T;
-	cin>>T;
-	
-	for(test_case = 1; test_case <= T; ++test_case)
-	{
+    int T;
+    cin>>T;
+     
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
         int N, M;
         cin >> N >> M;
-        
+         
         // 격자판 생성
         vector<vector<char>> map(N, vector<char>(M));
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++)
                 cin >> map[i][j];
         }
-
+ 
         // 격자판 검사
         string answer;
         int board[4] = { 0, };
-
+ 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < M; j++){
                 if(map[i][j] == '#'){
                     if((i + j) % 2 == 0) 
                         board[0]++;
-                    else 
+                    else
                         board[1]++;
                 }
                 else if(map[i][j] == '.'){
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
                 }
             }
         }
-
+ 
         if(board[0] > 0 && board[1] > 0)
             answer = "impossible";
         else if(board[0] > 0 && board[2] > 0)
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
             answer = "impossible";
         else
             answer = "possible";
-
+ 
         cout << "#" << test_case << " " << answer << endl;
     }
     return 0;
